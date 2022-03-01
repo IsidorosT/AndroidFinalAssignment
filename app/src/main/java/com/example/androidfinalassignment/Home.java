087315f70e0ba00bf175be250409988da33a0f45
IsidorosT <is.tsalapatis@gmail.com>
2022-03-01 09:28:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/androidfinalassignment/Home.java
@@ -0,0 +1,20 @@
+package com.example.androidfinalassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayout.Tab;
+
+public class Home extends AppCompatActivity {
+
+    public TabLayout TabBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/androidfinalassignment/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.androidfinalassignment;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    public TextView UsernameText;
+    public TextView PasswordText;
+    public Button LoginButton;
+
+    private FirebaseAuth auth;
+    FirebaseUser user;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        UsernameText = findViewById(R.id.UsernameText);
+        PasswordText = findViewById(R.id.PasswordText);
+        LoginButton = findViewById(R.id.LoginButton);
+        auth = FirebaseAuth.getInstance();
+
+        LoginButton.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        String username = "";
+                        String password = "";
+                        if(UsernameText.getText() != null)
+                            username = UsernameText.getText().toString();
+                        if(PasswordText.getText() != null)
+                            password = PasswordText.getText().toString();
+                        if(!username.equals("") || !password.equals(""))
+                        {
+                            auth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task)
+                                {
+                                    if (task.isSuccessful())
+                                    {
+                                        user = auth.getCurrentUser();
+                                        Intent map = new Intent(MainActivity.this,Home.class);
+                                        startActivity(map);
+                                    }
+                                    else {
+                                        user = null;
+                                        Intent map = new Intent(MainActivity.this,MainActivity.class);
+                                        startActivity(map);
+                                    }
+                                }
+                            });
+                        }
+
+
+                    }
+                }
+        );
+    }
+}
