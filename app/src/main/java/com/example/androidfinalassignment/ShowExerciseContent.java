ffd85bc66d492d9985f05e1f368bf7cbf79e20a8
IsidorosT <is.tsalapatis@gmail.com>
2022-03-08 11:14:54 +0200

    PATRA COMMIT

--- app/src/main/java/com/example/androidfinalassignment/ShowExerciseContent.java
+++ app/src/main/java/com/example/androidfinalassignment/ShowExerciseContent.java
@@ -1,11 +1,18 @@
 package com.example.androidfinalassignment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -14,23 +21,37 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.google.gson.Gson;
 
+import org.apache.tika.language.LanguageIdentifier;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
+
+
 
 public class ShowExerciseContent extends AppCompatActivity {
 
     ExerciseContent Content;
     Button BackButton;
     Button SubmitButton;
-
+    TextView LocationView;
     Object[] Exercisekeys;
     List<EditText> Answers = new LinkedList<>();
+    SpeechRecognizer speechRecognizer;
 
+    List<Boolean> ActiveSounds = new LinkedList<>();
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    LocationManager locationManager;
     public Context getContext(){
         return this.getApplicationContext();
     }
@@ -38,6 +59,39 @@ public class ShowExerciseContent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_exercise_content);
+        //We taking Record Audio Permissions for Speech Recognition - SpeechToText
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},101);
+        }
+        if (checkCallingOrSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 101);
+            }
+        }
+        LocationView = findViewById(R.id.locationView);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        location loc = new location();
+        /*
+        String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+        if(!provider.contains("gps")){ //if gps is disabled
+            final Intent poke = new Intent();
+            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            sendBroadcast(poke);
+        }
+
+         */
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 0, loc);
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -89,6 +143,88 @@ public class ShowExerciseContent extends AppCompatActivity {
             EditText answer = new EditText(this.getApplicationContext());
             lay.addView(answer);
             Answers.add(answer);
+            LanguageIdentifier identifier = new LanguageIdentifier(question.getText().toString());
+            if(identifier.getLanguage().equalsIgnoreCase("el")){
+                SpeechRecognizer speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+                final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                //Here we implement the listener for Speech Recognition - SpeechToText
+                speechRecognizer.setRecognitionListener(new RecognitionListener() {
+                    @Override
+                    public void onReadyForSpeech(Bundle params) {
+
+                    }
+
+                    @Override
+                    public void onBeginningOfSpeech() {
+
+                    }
+
+                    @Override
+                    public void onRmsChanged(float rmsdB) {
+
+                    }
+
+                    @Override
+                    public void onBufferReceived(byte[] buffer) {
+
+                    }
+
+                    @Override
+                    public void onEndOfSpeech() {
+
+                    }
+
+                    @Override
+                    public void onError(int error) {
+                        System.out.println("onError"+error);
+                    }
+
+                    @Override
+                    public void onResults(Bundle results) {
+                        //Here we taking results from voice
+                        ArrayList<String> data = results.getStringArrayList(speechRecognizer.RESULTS_RECOGNITION);
+
+                        answer.setText(answer.getText() + " " +data.get(0));
+                    }
+
+                    @Override
+                    public void onPartialResults(Bundle partialResults) {
+                        ArrayList<String> data = partialResults.getStringArrayList(speechRecognizer.RESULTS_RECOGNITION);
+
+                        answer.setText(answer.getText() + " " +data.get(0));
+                    }
+
+                    @Override
+                    public void onEvent(int eventType, Bundle params) {
+
+                    }
+                });
+                Button MicButton = new Button(this.getApplicationContext());
+                MicButton.setText("TestSpeech");
+                MicButton.setBackgroundColor(Color.LTGRAY);
+                ActiveSounds.add(false);
+                final int x = ActiveSounds.size()-1;
+                //This is the Mic Button which enables and disables speechToText
+                MicButton.setOnClickListener(
+                        new View.OnClickListener() {
+                            public void onClick(View v) {
+                                if(ActiveSounds.get(x)){
+                                    //Stop Listening
+                                    speechRecognizer.stopListening();
+                                    MicButton.setBackgroundColor(Color.LTGRAY);
+                                    ActiveSounds.set(x,false);
+                                }else{
+                                    //Start Listening
+                                    speechRecognizer.startListening(speechRecognizerIntent);
+                                    MicButton.setBackgroundColor(Color.RED);
+                                    ActiveSounds.set(x,true);
+                                }
+                            }
+                        }
+                );
+                lay.addView(MicButton);
+            }
             layout.addView(lay);
         }
         SubmitButton = findViewById(R.id.SubmitButton);
@@ -130,5 +266,32 @@ public class ShowExerciseContent extends AppCompatActivity {
                     }
                 }
         );
+
+
+
+    }
+    class location implements LocationListener {
+        Boolean one = true;
+        private final DecimalFormat df = new DecimalFormat("0.00");
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if(one){
+                String latitude = df.format(location.getLatitude());
+                String longitude = df.format(location.getLongitude());
+                LocationView.setText("Location\nLatitude: "+ latitude + "\nLongitude: "+longitude);
+                one = false;
+            }
+
+        }
+        @Override
+        public void onProviderDisabled(String arg0) {}
+        @Override
+        public void onProviderEnabled(String arg0) {}
+
+
+
+        @Override
+        public void onStatusChanged(String arg0, int arg1, Bundle arg2) {}
+
     }
 }
